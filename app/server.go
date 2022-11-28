01ac97ee5760d351503d9a47cee04530e9e15a19
dmytzo <dmitriytris@gmail.com>
2022-11-29 00:41:28 +0100

    Add initial PONG command

--- app/server.go
+++ app/server.go
@@ -2,21 +2,63 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
-	"os"
+	"time"
 )
 
+const (
+	CommandPING = "PING"
+
+	SignString = "+"
+)
+
+func strb(s string) []byte {
+	return []byte(fmt.Sprintf("%s%s\r\n", SignString, s))
+}
+
 func main() {
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("redis")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		log.Fatalf("bind to port: %s", err.Error())
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatalf("accept connection: %s", err.Error())
+		}
+
+		c.SetReadDeadline(time.Now().Add(10 * time.Second))
+
+		content := make([]byte, 1024)
+		if _, err := c.Read(content); err != nil {
+			log.Fatalf("read: %s", err.Error())
+
+		}
+
+		if _, err := c.Write(strb("PONG")); err != nil {
+			log.Fatalf("write: %s", err.Error())
+		}
+
+		//if err := c.Close(); err != nil {
+		//	log.Fatalf("close: %s", err.Error())
+		//}
+		//switch string(content) {
+		//case CommandPING:
+		//	fmt.Println("herre")
+		//	if _, err := c.Write(strb("PONG")); err != nil {
+		//		log.Fatalf("write: %s", err.Error())
+		//	}
+		//}
+		//
+		//if err := c.Close(); err != nil {
+		//	log.Fatalf("close: %s", err.Error())
+		//}
 	}
+
 }
