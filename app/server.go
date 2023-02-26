fd1a3644482f9b7e36afe932f7d31e9ea2b45526
dmytzo <dmitriytris@gmail.com>
2023-02-26 16:19:24 +0100

    Add command parser

--- app/server.go
+++ app/server.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 )
 
 const (
 	CommandPING = "PING"
+	CommandECHO = "ECHO"
+	CommandGET  = "GET"
+	CommandSET  = "SET"
+
+	SignRespArray      = '*'
+	SignRespBulkString = '$'
 
 	SignString = "+"
 )
-
-func strb(s string) []byte {
-	return []byte(fmt.Sprintf("%s%s\r\n", SignString, s))
-}
 
 func main() {
 	fmt.Println("redis")
@@ -27,6 +33,8 @@ func main() {
 
 	defer l.Close()
 
+	storage := map[string]string{}
+
 	for {
 		c, err := l.Accept()
 		if err != nil {
@@ -35,34 +43,101 @@ func main() {
 
 		go func() {
 			for {
-				buf := make([]byte, 1024)
-				if _, err := c.Read(buf); err != nil {
-					if err == io.EOF {
-						break
+				reader := bufio.NewReader(c)
+
+				commands, err := parse(reader)
+
+				if err != nil {
+					if errors.Is(err, io.EOF) {
+						continue
 					}
 
-					log.Fatalf("read: %s", err.Error())
+					log.Fatalf("read input: %s", err.Error())
 				}
 
-				if _, err := c.Write(strb("PONG")); err != nil {
+				if len(commands) == 0 {
+					continue
+				}
+
+				var resp string
+
+				switch strings.ToUpper(commands[0]) {
+				case CommandPING:
+					resp = "PONG"
+				case CommandECHO:
+					resp = strings.Join(commands[1:], " ")
+				case CommandSET:
+					if len(commands) < 3 {
+						continue
+					}
+
+					storage[commands[1]] = commands[2]
+					resp = "OK"
+				case CommandGET:
+					if len(commands) < 2 {
+						continue
+					}
+
+					resp = storage[commands[1]]
+				}
+
+				if _, err := c.Write(strb(resp)); err != nil {
 					log.Fatalf("write: %s", err.Error())
 				}
 			}
 		}()
-
-		//if err := c.Close(); err != nil {
-		//	log.Fatalf("close: %s", err.Error())
-		//}
-		//switch string(content) {
-		//case CommandPING:
-		//	fmt.Println("herre")
-		//	if _, err := c.Write(strb("PONG")); err != nil {
-		//		log.Fatalf("write: %s", err.Error())
-		//	}
-		//}
-		//
-		//if err := c.Close(); err != nil {
-		//	log.Fatalf("close: %s", err.Error())
-		//}
 	}
+}
+
+func strb(s string) []byte {
+	return []byte(fmt.Sprintf("%s%s\r\n", SignString, s))
+}
+
+type cmd []string
+
+func parse(b *bufio.Reader) (cmd, error) {
+	respType, err := b.ReadByte()
+	if err != nil {
+		return nil, fmt.Errorf("read byte: %w", err)
+	}
+
+	itemNumRaw, err := b.ReadBytes('\n')
+	if err != nil {
+		return nil, fmt.Errorf("read bytes: %w", err)
+	}
+
+	itemNum, err := strconv.Atoi(string(itemNumRaw[:len(itemNumRaw)-2]))
+	if err != nil {
+		return nil, fmt.Errorf("strconv atoi: %w", err)
+	}
+
+	var command cmd
+
+	switch respType {
+	case SignRespArray:
+		for i := 0; i < itemNum; i++ {
+			cmds, err := parse(b)
+			if err != nil {
+				return nil, fmt.Errorf("parse: %w", err)
+			}
+
+			command = append(command, cmds...)
+		}
+		return command, nil
+
+	case SignRespBulkString:
+		itemRaw, err := b.ReadBytes('\n')
+		if err != nil {
+			return nil, fmt.Errorf("peak: %w", err)
+		}
+
+		item := itemRaw[:len(itemRaw)-2]
+		if itemLen := len(item); itemLen != itemNum {
+			return nil, fmt.Errorf("wrond len: %d != %d", itemLen, itemNum)
+		}
+
+		return cmd{string(item)}, nil
+	}
+
+	return command, nil
 }
